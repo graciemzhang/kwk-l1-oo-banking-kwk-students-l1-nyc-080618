1213092761ccdf472ca3144006e91ec566ba33f9
graciemzhang <gracie.zhang@lps-students.org>
2018-08-10 16:29:22 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,49 @@
 class BankAccount
-  # code here
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def name
+    return @name
+  end
+  
+  def balance
+    return @balance
+  end
+  
+  def balance=(new_balance)
+    @balance = new_balance
+  end
+  
+  def status
+    return @status
+  end
+  
+  def status=(new_status)
+    @status = new_status
+  end
+  
+  def deposit(value)
+    @balance += value
+  end
+  
+  def display_balance
+    return "Your Balance is $#{@balance}."
+  end
+  
+  def valid?
+    if @status == "open" && @balance > 0 
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # code here
+  def initialize(sender, receiver, transfered)
+    @sender = sender
+    @receiver = receiver
+    @transfered = transfered
+    @status = "pending"
+  end 
+  
+  def sender 
+    return @sender
+  end
+  
+  def receiver
+    return @receiver
+  end
+  
+  def status
+    return @status
+  end
+  
+  def status=(new_status)
+    @status = new_status
+  end
+  
 end
